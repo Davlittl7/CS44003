144a795b76b8e6d3e430a1c5b37e130f1f8b9385
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-02-20 22:22:06 -0500

    Update Utility.swift

--- MatchEmScene/MatchEmScene/Utility.swift
+++ MatchEmScene/MatchEmScene/Utility.swift
@@ -1,1 +1,40 @@
+//Created by Davonte Littleton
 
+
+import UIKit
+
+class Utility: NSObject {
+  let ri = Int.random(in: 0 ... 99)
+  let rd = Double.random(in: 0.0 ... 1.0)
+  // MARK: - ==== Random Value Funcs ====
+  //================================================
+  static func randomFloatZeroThroughOne() -> CGFloat {
+    // Get a random value
+    let randomFloat = CGFloat.random(in: 0 ... 1.0)
+    return randomFloat
+  }
+  
+  //================================================
+  static func getRandomSize(fromMin min: CGFloat,
+  throughMax max: CGFloat) -> CGSize {
+    // Create a random CGSize
+    let randWidth = randomFloatZeroThroughOne() * (max - min) + min
+    let randHeight = randomFloatZeroThroughOne() * (max - min) + min
+    let randSize = CGSize(width: randWidth, height: randHeight)
+    return randSize
+  }
+  
+  //================================================
+  static func getRandomLocation(size rectSize: CGSize,
+  screenSize: CGSize) -> CGPoint {
+    // Get the screen dimensions
+    let screenWidth = screenSize.width
+    let screenHeight = screenSize.height
+    // Create a random location/point
+    let rectX = randomFloatZeroThroughOne() * (screenWidth - rectSize.width)
+    let rectY = randomFloatZeroThroughOne() * (screenHeight - rectSize.height)
+    let location = CGPoint(x: rectX, y: rectY)
+    return location
+  }
+
+}
